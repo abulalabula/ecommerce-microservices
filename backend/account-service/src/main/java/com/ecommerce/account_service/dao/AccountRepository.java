72b46d4235fece5cb3cd61db9e5f47233cc58f7b
abulalabula <abulalabula@gmail.com>
2025-02-20 22:34:24 -0800

    Update exception for existing and access limit

--- backend/account-service/src/main/java/com/ecommerce/account_service/dao/AccountRepository.java
+++ backend/account-service/src/main/java/com/ecommerce/account_service/dao/AccountRepository.java
@@ -2,9 +2,23 @@ package com.ecommerce.account_service.dao;
 
 import com.ecommerce.account_service.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
-//    Optional<Account> findByUserEmail(String userEmail);
+
+    // for authentication
+    Optional<Account> findByUserEmail(String email);
+
+    // login
+    Optional<Account> findByUserName(String userName);
+
+    // email is already registered?
+    Boolean existsByUserEmail(String email);
+
+    // username is already used?
+    Boolean existsByUserName(String userName);
+
+    // for deletion and updates
+//    boolean existsById(Long id);
 }
--- backend/account-service/src/main/java/com/ecommerce/account_service/service/impl/AddressServiceImpl.java
+++ backend/account-service/src/main/java/com/ecommerce/account_service/service/impl/AddressServiceImpl.java
@@ -2,6 +2,8 @@ package com.ecommerce.account_service.service.impl;
 
 import com.ecommerce.account_service.dao.AddressRepository;
 import com.ecommerce.account_service.entity.Address;
+import com.ecommerce.account_service.exception.DuplicateResourceException;
+import com.ecommerce.account_service.exception.ResourceNotFoundException;
 import com.ecommerce.account_service.payload.AddressDTO;
 import com.ecommerce.account_service.service.AddressService;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,14 @@ public class AddressServiceImpl implements AddressService {
 
     @Override
     public AddressDTO createAddress(AddressDTO addressDTO) {
+
+        boolean addressExists = addressRepository.existsByStreetAndCityAndStateAndZipCode(
+                addressDTO.getStreet(), addressDTO.getCity(), addressDTO.getState(), addressDTO.getZipCode());
+
+        if (addressExists) {
+            throw new DuplicateResourceException("Address already exists at " + addressDTO.getStreet());
+        }
+
         Address address = new Address(
                 null, // ID auto-generated
                 addressDTO.getStreet(),
@@ -63,6 +73,9 @@ public class AddressServiceImpl implements AddressService {
 
     @Override
     public void deleteAddress(Long id) {
+        if (!addressRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Address with ID: " + id + " not found.");
+        }
         addressRepository.deleteById(id);
     }
 
