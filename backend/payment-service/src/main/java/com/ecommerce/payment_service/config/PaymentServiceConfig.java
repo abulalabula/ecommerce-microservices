96b8918843264814f5dbfe1f4785166739833f4f
abulalabula <abulalabula@gmail.com>
2025-02-26 17:24:38 -0800

    Update consumer logic and add ServiceConfig and exist bugs in dockerize

---
+++ backend/payment-service/src/main/java/com/ecommerce/payment_service/config/PaymentServiceConfig.java
@@ -0,0 +1,42 @@
+package com.ecommerce.payment_service.config;
+
+import com.ecommerce.payment_service.payload.PaymentRequestDto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class PaymentServiceConfig {
+
+    @Bean
+    public ConsumerFactory<String, PaymentRequestDto> paymentRequestConsumerFactory() {
+        JsonDeserializer<PaymentRequestDto> deserializer = new JsonDeserializer<>(PaymentRequestDto.class, false);
+        deserializer.addTrustedPackages("com.ecommerce.payment_service.payload");
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-group");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, PaymentRequestDto> paymentRequestKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, PaymentRequestDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(paymentRequestConsumerFactory());
+        return factory;
+    }
+}
